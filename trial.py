26babee88fd5ed4e0eaab0557394d49c25dff985
Samaksh Goyal <mailsamakshgoyal@gmail.com>
2019-07-12 01:11:10 -0700

    reading from the workspace

--- trial.py
+++ trial.py
@@ -1,134 +1,110 @@
+import os
 import json
 import requests
 import feedparser
 import jsonpickle
+from slackclient import SlackClient
 
 
 def get_real_content(current_summary):
     return current_summary.split("<br />", 1)[1]
 
 
+# def get_channel_id(channel_name):
+#     channels = get_channels()  # using channels.list
+#     for channel in channels:
+#         if channel["name"] == channel_name:
+#             return channel["id"]
+
+# def write_recent_Time():
+
+
+# def read_recent_Time():
+
+# def get_most_recentTime():
+
 def main():
-    print("Hello")
 
-    # url = 'https://hooks.slack.com/services/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'
-    # payload = '{ "type": "section", "text": { "type": "plain_text", "text": "This is a plain text section block.", "emoji": false } }, { "type": "section", "text": { "type": "plain_text", "text": "This is a plain text section block.", "emoji": false } }'
-    # headers = {'Content-type': 'application/json'}
-    # # curl - X POST - H 'Content-type: application/json' - -data '{"text":"Hello, World!"}'
-    # # https: // hooks.slack.com/services/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
-
-    # for i in range(10):
-    #     res = requests.post(url, data=payload, headers=headers)
-
-    # print(res.text)
+    SLACK_TOKEN = XXXX
+    slack_client = SlackClient(SLACK_TOKEN)
 
     wekbook_url = 'https://hooks.slack.com/services/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'
-
-    # data = {
-    #     "attachments": [
-    #         {
-    #             "fallback": "Required plain-text summary of the attachment.",
-    #             "color": "#36a64f",
-    #             "pretext": "Optional text that appears above the attachment block",
-    #             "author_name": "Bobby Tables",
-    #             "author_link": "http://flickr.com/bobby/",
-    #             "author_icon": "http://flickr.com/icons/bobby.jpg",
-    #             "title": "Slack API Documentation",
-    #             "title_link": "https://api.slack.com/",
-    #             "text": "Optional text that appears within the attachment",
-    #             "image_url": "http://my-website.com/path/to/image.jpg",
-    #             "thumb_url": "http://example.com/path/to/thumb.png",
-    #             "footer": "Slack API",
-    #             "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-    #             "ts": 123456789
-    #         }
-    #     ]
-    # }
-
-    # data = {
-    #     "attachments": [
-    #         {
-    #             "author_name": "Candido Dessanti",
-    #             "text": "Ok I'll try to reproduce with such cardinalities, and report back",
-    #             "title": "Facing Issue : ALLOCATION failed to find 256000000B free.",
-    #             "title_link": "https://community.omnisci.com/communities/community-home/digestviewer/viewthread?GroupId=13&MessageKey=67",
-    #             "color": "#3AA3E3",
-    #             "attachment_type": "default",
-    #             "fields": [
-    #                 {
-    #                     "title": "Project",
-    #                     "value": "Awesome Project"
-    #                 },
-    #                 {
-    #                     "title": "Environment",
-    #                     "value": "production"
-    #                 }
-    #             ]
-    #         }
-    #     ]
-    # }
 
     NewsFeed = feedparser.parse(
         "https://community.omnisci.com/rssgenerator?UserKey=7f2de571-92e8-49b0-ba12-27413bf99c95")
 
+    channels = slack_client.api_call("channels.list")
+    channel_id = None
+    for channel in channels['channels']:
+        print(channel['name'])
+        if channel['name'] == "memes":
+            print("here")
+            channel_id = channel['id']
+
+    res = slack_client.api_call(
+        "channels.history", channel=channel_id, count=200)
+    #history = slack_client.api_call("channels.history", channel=channel_id, oldest=oldest, latest=latest)
+
     count = 0
-    for entry in NewsFeed['entries']:
-
-        data = {}
-        data["attachments"] = []
-
-        extras = []
-        extras.append({
-            "title": "Date",
-            "value": entry['published'],
-            "short": True
-        })
-        extras.append({
-            "title": "Date2",
-            "value": entry['published'],
-            "short": True
-        })
-
-        data["attachments"].append({
-            "author_name": entry['author'],
-            "text": get_real_content(entry['summary']),
-            "title": entry['title'],
-            "title_link": entry['link'],
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "fields": extras,
-        })
-
-        # "author_name": "Candido Dessanti",
-        # "text": "Ok I'll try to reproduce with such cardinalities, and report back",
-        # "title": "Facing Issue : ALLOCATION failed to find 256000000B free.",
-        # "title_link": "https://community.omnisci.com/communities/community-home/digestviewer/viewthread?GroupId=13&MessageKey=67",
-        # "color": "#3AA3E3",
-        # "attachment_type": "default",
-
-        print("hi")
-
-        requests.post(wekbook_url, data=json.dumps(
-            data), headers={'Content-Type': 'application/json'})
-
-        if (count == 4):
-            break
-
+    for mess in res['messages']:
+        # print(mess)
+        # break
+        try:
+            if (mess['subtype'] == "bot_message"):
+                print("here")
+                print(count)
+                break
+        except:
+            pass
         count += 1
 
-        del data
-        del extras
+    # print(res['messages'][1]['subtype'] == "bot_message")
 
-    print(json.dumps(data, indent=4))
+    # count = 0
+    # for entry in NewsFeed['entries']:
+
+    #     data = {}
+    #     data["attachments"] = []
+
+    #     extras = []
+    #     extras.append({
+    #         "title": "Date",
+    #         "value": entry['published'],
+    #         "short": True
+    #     })
+    #     extras.append({
+    #         "title": "Date2",
+    #         "value": entry['published'],
+    #         "short": True
+    #     })
+
+    #     data["attachments"].append({
+    #         "author_name": entry['author'],
+    #         "text": get_real_content(entry['summary']),
+    #         "title": entry['title'],
+    #         "title_link": entry['link'],
+    #         "color": "#3AA3E3",
+    #         "attachment_type": "default",
+    #         "fields": extras,
+    #     })
+
+    #     response = requests.post(wekbook_url, data=json.dumps(
+    #         data), headers={'Content-Type': 'application/json'})
+
+    #     # print(json.dumps(data, indent=4))
+    #     print('Response: ' + str(response.text))
+    #     print('Response code: ' + str(response.status_code))
+
+    #     if (count == 0):
+    #         break
+
+    #     count += 1
+
+    #     del data
+    #     del extras
 
     # with open('myJsonFun/input.json', 'r') as f:
     #     data = json.load(f)
-
-    # response = requests.post(wekbook_url, data=json.dumps(
-    #     data), headers={'Content-Type': 'application/json'})
-
-    # print('Response: ' + str(response.text))
-    # print('Response code: ' + str(response.status_code))
 
 
 if __name__ == "__main__":

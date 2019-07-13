b090164868b027216c77986f2a65bd6713344983
Samaksh Goyal <mailsamakshgoyal@gmail.com>
2019-07-13 01:21:53 -0700

    using blocks now

--- trial.py
+++ trial.py
@@ -56,17 +56,84 @@ def community_time(feed):
 
 def main():
 
-    SLACK_TOKEN = os.environ["SLACK_API_TOKEN"]
-    slack_client = SlackClient(SLACK_TOKEN)
+    # SLACK_TOKEN = os.environ["SLACK_API_TOKEN"]
+    # slack_client = SlackClient(SLACK_TOKEN)
 
-    webhook_url = 'https://hooks.slack.com/services/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'
+    # webhook_url = 'https://hooks.slack.com/services/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'
 
     community_page = feedparser.parse(
         "https://community.omnisci.com/rssgenerator?UserKey=7f2de571-92e8-49b0-ba12-27413bf99c95")
 
+    # channels_list = slack_client.api_call("channels.list")
+    # result = slack_client.api_call(
+    #     "channels.history", channel=get_channel_id("memes", channels_list), count=100)
+
+    slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
     channels_list = slack_client.api_call("channels.list")
-    result = slack_client.api_call(
-        "channels.history", channel=get_channel_id("memes", channels_list), count=100)
+    # print(channels_list)
+
+    # slack_client.api_call(
+    #     "chat.postMessage",
+    #     channel=get_channel_id("memes", channels_list),
+    #     text="My message",
+    #     blocks=[
+    #         {
+    #             "type": "section",
+    #             "block_id": "section567",
+    #             "text": {
+    #                 "type": "mrkdwn",
+    #                     "text": "This is a section block with an accessory image."
+    #             },
+    #             "accessory": {
+    #                 "type": "image",
+    #                 "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+    #                 "alt_text": "cute cat"
+    #             }
+    #         }
+    #     ]
+    # )
+
+    # ws_time = datetime.datetime.min  # workspace_time(result)
+    # comm_time = community_time(community_page)
+
+    # if(comm_time > ws_time):
+
+    #     for entry in reversed(community_page['entries']):
+    #         post_time = get_date(entry['published_parsed'])
+
+    #         # You only need to check posts that were made most recently relative to the ones that
+    #         # have already been posted on slack
+    #         if(post_time > ws_time):
+
+    #             data = {}
+    #             data["attachments"] = []
+
+    #             extras = []
+    #             extras.append({
+    #                 "title": "Post Date",
+    #                 "value": post_time.strftime('%m/%d/%y %H:%M:%S'),
+    #                 "short": True
+    #             })
+
+    #             data["attachments"].append({
+    #                 "author_name": entry['author'],
+    #                 "text": get_real_content(entry['summary']),
+    #                 "title": entry['title'],
+    #                 "title_link": entry['link'],
+    #                 "color": "#3AA3E3",
+    #                 "attachment_type": "default",
+    #                 "fields": extras,
+    #             })
+
+    #             response = requests.post(webhook_url, data=json.dumps(
+    #                 data), headers={'Content-Type': 'application/json'})
+
+    #             # print(json.dumps(data, indent=4))
+    #             print('Response: ' + str(response.text))
+    #             print('Response code: ' + str(response.status_code))
+
+    #             del data
+    #             del extras
 
     ws_time = datetime.datetime.min  # workspace_time(result)
     comm_time = community_time(community_page)
@@ -79,36 +146,55 @@ def main():
             # You only need to check posts that were made most recently relative to the ones that
             # have already been posted on slack
             if(post_time > ws_time):
+                myblock = [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text":  "*" + entry['title'] + "*"
+                        },
+                        "accessory": {
+                            "type": "button",
+                            "text": {
+                                    "type": "plain_text",
+                                "text": "OriginalPost"
+                            },
+                            "url": entry['link']
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                                "type": "mrkdwn",
+                            "text": get_real_content(entry['summary'])
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                                {
+                                    "type": "mrkdwn",
+                                    "text": "Last updated: " + post_time.strftime('%m/%d/%y %H:%M:%S')
+                                }
+                        ]
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                                {
+                                    "type": "mrkdwn",
+                                    "text": "*Author:* " + entry['author']
+                                }
+                        ]
+                    }
+                ]
 
-                data = {}
-                data["attachments"] = []
-
-                extras = []
-                extras.append({
-                    "title": "Post Date",
-                    "value": post_time.strftime('%m/%d/%y %H:%M:%S'),
-                    "short": True
-                })
-
-                data["attachments"].append({
-                    "author_name": entry['author'],
-                    "text": get_real_content(entry['summary']),
-                    "title": entry['title'],
-                    "title_link": entry['link'],
-                    "color": "#3AA3E3",
-                    "attachment_type": "default",
-                    "fields": extras,
-                })
-
-                response = requests.post(webhook_url, data=json.dumps(
-                    data), headers={'Content-Type': 'application/json'})
-
-                # print(json.dumps(data, indent=4))
-                print('Response: ' + str(response.text))
-                print('Response code: ' + str(response.status_code))
-
-                del data
-                del extras
+                slack_client.api_call(
+                    "chat.postMessage",
+                    channel=get_channel_id("memes", channels_list),
+                    text="My message",
+                    blocks=myblock
+                )
 
     # with open('myJsonFun/input.json', 'r') as f:
     #     data = json.load(f)

9d78d5091ba94e1b1eaea675dc887e28a81604ea
Samaksh Goyal <mailsamakshgoyal@gmail.com>
2019-07-15 10:26:08 -0700

    cleaning stuff, finding by bot name

--- trial.py
+++ trial.py
@@ -24,38 +24,11 @@ def get_channel_id(channel_name, channels_list):
             return channel["id"]
 
 
-# Time from Slack Workspace
-# def workspace_time(result):
-
-    # index = 0
-    # for message in result['messages']:
-    #     try:
-    #         if (message['subtype'] == "bot_message"):
-    #             break
-    #     except:
-    #         pass
-    #     index += 1
-
-    # # In the event the bot has never been used
-    # if(index == len(result['messages'])):
-    #     corrected_time = datetime.datetime.min
-    # else:
-    #     time = result['messages'][index]['attachments'][0]['fields'][1]['value']
-    #     corrected_time = datetime.datetime.strptime(time, '%m/%d/%y %H:%M:%S')
-
-    # return corrected_time
-
-    # Time from Community Page
-
-def workspace_time(results, mybotname):
-
-    allMessages = results['messages']
-
+def workspace_time(history, bot_username):
+    all_messages = history['messages']
     time = None
-
-    for message in allMessages:
-        if (message['username'] == mybotname):
-            print(message['blocks'])
+    for message in all_messages:
+        if (message['username'] == bot_username):
             for block in message['blocks']:
                 if(block['block_id'] == "DateSection"):
                     time = block['elements'][0]['text'].split("*Date:* ", 1)[1]
@@ -74,170 +47,80 @@ def community_time(feed):
 
 def main():
 
-    # SLACK_TOKEN = os.environ["SLACK_API_TOKEN"]
-    # slack_client = SlackClient(SLACK_TOKEN)
-
-    # webhook_url = 'https://hooks.slack.com/services/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'
-
-    slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-    mybotname = "memeLord"
-
     community_page = feedparser.parse(
         "https://community.omnisci.com/rssgenerator?UserKey=7f2de571-92e8-49b0-ba12-27413bf99c95")
 
-    # channels_list = slack_client.api_call("channels.list")
-
-    auth = slack_client.api_call("auth.test")["user_id"]
-    print(auth)
+    slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+    bot_username = "memeLord"
 
     channels_list = slack_client.api_call("channels.list")
+    history = slack_client.api_call(
+        "channels.history", channel=get_channel_id("memes", channels_list), count=100)
 
-    # https://api.slack.com/methods/bots.info/test
-    result = slack_client.api_call(
-        "channels.history", channel=get_channel_id("memes", channels_list), count=3)
-    print(result)
+    ws_time = workspace_time(history, bot_username)  # datetime.datetime.min
+    comm_time = community_time(community_page)
 
-    print("here")
-    mybotinfo = slack_client.api_call("bots.info", bot="BLFFE0B71")
-    print(mybotinfo)
+    if(comm_time > ws_time):
 
-    print("spacer")
-    print("spacer")
+        for entry in reversed(community_page['entries']):
+            post_time = get_date(entry['published_parsed'])
 
-    print(workspace_time(result, mybotname))
+            # You only need to check posts that were made most recently relative to the ones that
+            # have already been posted on slack
+            if(post_time > ws_time):
+                myblock = [
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text":  "*" + entry['title'] + "*"
+                        },
+                        "accessory": {
+                            "type": "button",
+                            "text": {
+                                    "type": "plain_text",
+                                "text": "Original Post"
+                            },
+                            "url": entry['link']
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                                "type": "mrkdwn",
+                            "text": get_real_content(entry['summary'])
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "block_id": "DateSection",
+                        "elements": [
+                                {
+                                    "type": "mrkdwn",
+                                    "text": "*Date:* " + post_time.strftime('%a %b %d %Y %H:%M:%S')
+                                }
+                        ]
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                                {
+                                    "type": "mrkdwn",
+                                    "text": "*Author:* " + entry['author']
+                                }
+                        ]
+                    }
+                ]
 
-    # print(channels_list)
-
-    # slack_client.api_call(
-    #     "chat.postMessage",
-    #     channel=get_channel_id("memes", channels_list),
-    #     text="My message",
-    #     blocks=[
-    #         {
-    #             "type": "section",
-    #             "block_id": "section567",
-    #             "text": {
-    #                 "type": "mrkdwn",
-    #                     "text": "This is a section block with an accessory image."
-    #             },
-    #             "accessory": {
-    #                 "type": "image",
-    #                 "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
-    #                 "alt_text": "cute cat"
-    #             }
-    #         }
-    #     ]
-    # )
-
-    # ws_time = datetime.datetime.min  # workspace_time(result)
-    # comm_time = community_time(community_page)
-
-    # if(comm_time > ws_time):
-
-    #     for entry in reversed(community_page['entries']):
-    #         post_time = get_date(entry['published_parsed'])
-
-    #         # You only need to check posts that were made most recently relative to the ones that
-    #         # have already been posted on slack
-    #         if(post_time > ws_time):
-
-    #             data = {}
-    #             data["attachments"] = []
-
-    #             extras = []
-    #             extras.append({
-    #                 "title": "Post Date",
-    #                 "value": post_time.strftime('%m/%d/%y %H:%M:%S'),
-    #                 "short": True
-    #             })
-
-    #             data["attachments"].append({
-    #                 "author_name": entry['author'],
-    #                 "text": get_real_content(entry['summary']),
-    #                 "title": entry['title'],
-    #                 "title_link": entry['link'],
-    #                 "color": "#3AA3E3",
-    #                 "attachment_type": "default",
-    #                 "fields": extras,
-    #             })
-
-    #             response = requests.post(webhook_url, data=json.dumps(
-    #                 data), headers={'Content-Type': 'application/json'})
-
-    #             # print(json.dumps(data, indent=4))
-    #             print('Response: ' + str(response.text))
-    #             print('Response code: ' + str(response.status_code))
-
-    #             del data
-    #             del extras
-
-    # ws_time = datetime.datetime.min  # workspace_time(result)
-    # comm_time = community_time(community_page)
-
-    # if(comm_time > ws_time):
-
-    #     for entry in reversed(community_page['entries']):
-    #         post_time = get_date(entry['published_parsed'])
-
-    #         # You only need to check posts that were made most recently relative to the ones that
-    #         # have already been posted on slack
-    #         if(post_time > ws_time):
-    #             myblock = [
-    #                 {
-    #                     "type": "divider"
-    #                 },
-    #                 {
-    #                     "type": "section",
-    #                     "text": {
-    #                         "type": "mrkdwn",
-    #                         "text":  "*" + entry['title'] + "*"
-    #                     },
-    #                     "accessory": {
-    #                         "type": "button",
-    #                         "text": {
-    #                                 "type": "plain_text",
-    #                             "text": "Original Post"
-    #                         },
-    #                         "url": entry['link']
-    #                     }
-    #                 },
-    #                 {
-    #                     "type": "section",
-    #                     "text": {
-    #                             "type": "mrkdwn",
-    #                         "text": get_real_content(entry['summary'])
-    #                     }
-    #                 },
-    #                 {
-    #                     "type": "context",
-    #                     "block_id": "DateSection",
-    #                     "elements": [
-    #                             {
-    #                                 "type": "mrkdwn",
-    #                                 "text": "*Date:* " + post_time.strftime('%a %b %d %Y %H:%M:%S')
-    #                             }
-    #                     ]
-    #                 },
-    #                 {
-    #                     "type": "context",
-    #                     "elements": [
-    #                             {
-    #                                 "type": "mrkdwn",
-    #                                 "text": "*Author:* " + entry['author']
-    #                             }
-    #                     ]
-    #                 }
-    #             ]
-
-    #             slack_client.api_call(
-    #                 "chat.postMessage",
-    #                 channel=get_channel_id("memes", channels_list),
-    #                 text="My message",
-    #                 blocks=myblock
-    #             )
-
-    # with open('myJsonFun/input.json', 'r') as f:
-    #     data = json.load(f)
+                slack_client.api_call(
+                    "chat.postMessage",
+                    channel=get_channel_id("memes", channels_list),
+                    text="My message",
+                    blocks=myblock
+                )
 
 
 if __name__ == "__main__":
